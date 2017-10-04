0ae8a8df6a5d9ce9198d0e8f45753ed95f76be94
anaezes <anaezes@gmail.com>
2017-10-04 16:36:42 +0100

    Forgot file

--- LAIGDemoParser/MyCircle.js
+++ LAIGDemoParser/MyCircle.js
@@ -29,12 +29,4 @@ MyCircle.prototype.initBuffers = function () {
     this.initGLBuffers();
 }
 
-MyCircle.prototype.setTexCoordsAmp = function (amplif_factor_S,amplif_factor_T) {
- /** this.texCoords = [
-    0, this.height/amplif_factor_T,
-    this.width/amplif_factor_S, this.height/amplif_factor_T,
-    this.width/amplif_factor_S, 0,
-    0,0
-  ];
-  this.updateTexCoordsGLBuffers();**/
-};
+MyCircle.prototype.setTexCoordsAmp = function (amplif_factor_S,amplif_factor_T) {};
