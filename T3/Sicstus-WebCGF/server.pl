1c5c8e9df28a9e667a3269556fb2c1e72797307e
Ana Santos <anaezes@gmail.com>
2017-12-17 18:38:16 +0000

    Added predicates to communicate with the prolog

--- T3/Sicstus-WebCGF/server.pl
+++ T3/Sicstus-WebCGF/server.pl
@@ -113,13 +113,31 @@ print_header_line(_).
 % Require your Prolog Files here
 
 parse_input(handshake, handshake).
+parse_input(test(C,N), Res) :- test(C,Res,N).
+parse_input(quit, goodbye).
 
 % Init game
-parse_input(gorogo, Msg) :- initGamePvP(Game),
-							Msg = Game. 
+parse_input(gorogo, Game) :- 
+	initGamePvP(Game). 
+
+% Get Play (bot)
+parse_input(getplay(Game,Turn), Play) :- 
+	getPlay(Game, Play, Turn).
+
+% Apply Play and get new Board 
+parse_input(applyPlay(Game, Play), NewGame) :-	
+	applyPlay(Game, Play, NewGameTmp),
+	updateGameCycle(NewGameTmp, NewGame).
+
+% verify if game has ended after each turn
+parse_input(endOfGame(Game, Winner), Winner) :- 
+	endOfGame(Game,Winner).
+
+
+
+%arse_input(play(Game), Play) :-	write(Game), Play = 'Pim'.
 
 /*
-parse_input([play, Game], Msg) :-	%getPlay(Game, Play, Turn),
 							applyPlay(Game,Play,GameTmp1),
 							updateGameCycle(GameTmp1,GameTmp2),
 							NextTurn is Turn+1,
